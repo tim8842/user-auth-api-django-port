7adda82586e4076a1d627eb562a9cb98597bc988
Stanko Dmitry <79981959+tim8842@users.noreply.github.com>
2024-09-09 17:13:53 +0700

    add cors. Close #8

--- user_auth/settings.py
+++ user_auth/settings.py
@@ -41,10 +41,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
     'users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware', 
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +55,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+#на будущее если буду делать фронт
+CORS_ALLOWED_ORIGINS = [
+    'https://example.com',
+    'https://sub.example.com',
 ]
 
 ROOT_URLCONF = "user_auth.urls"
