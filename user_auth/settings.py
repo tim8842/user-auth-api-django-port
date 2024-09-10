249ccc9c442375f030003aaacae785f10ae9b352
Stanko Dmitry <79981959+tim8842@users.noreply.github.com>
2024-09-10 12:05:51 +0700

    add secrets

--- user_auth/settings.py
+++ user_auth/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--tk)(-0_p%pny0&p%h278wrkb@x0@1k@@%t0=xe=i3kkh1q-#t"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
